16c27b7c2a88075f1a63eb564d008aeb41bfca7a
Walter Derezinski <walter@daringway.com>
2020-06-22 20:41:26 -0400

    added root alternate name

--- main-acm.tf
+++ main-acm.tf
@@ -15,9 +15,9 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_route53_record" "cert_validation" {
   count   = local.create_acm ? length(aws_acm_certificate.cert[0].domain_validation_options) : 0
   name    = aws_acm_certificate.cert[0].domain_validation_options.[count.index].resource_record_name
-  type    = "aws_acm_certificate.cert[0].domain_validation_options.[count.index].resource_record_type"
+  type    = aws_acm_certificate.cert[0].domain_validation_options.[count.index].resource_record_type
   zone_id = aws_route53_zone.default.zone_id
-  records = [ "aws_acm_certificate.cert[0].domain_validation_options.[count.index].resource_record_value" ]
+  records = [ aws_acm_certificate.cert[0].domain_validation_options.[count.index].resource_record_value ]
   ttl     = 60
   lifecycle {
     create_before_destroy = false
