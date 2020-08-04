3f2a1f2e3dc06ec1a58c9b84ab2fffff0b0ffd5f
Walter Derezinski <walter@daringway.com>
2020-08-03 23:59:32 -0400

    removed deprecated variables

--- variables-optional.tf
+++ variables-optional.tf
@@ -1,40 +1,4 @@
 
-variable enable_fastmail {
-  type        = bool
-  default     = false
-  description = "Configure DNS for fastmail.com"
-}
-
-variable fastmail_web_hostname {
-  type        = string
-  default     = "mail"
-  description = "the hostname to access fastmail, such as mail -> mail.acme.com. Set to null to disable"
-}
-
-variable fastmail_extra_spf {
-  type        = string
-  default     = ""
-  description = "If you need to add extra configuration information to the SPF record."
-}
-
-variable enable_fastmail_mailchimp {
-  type        = bool
-  default     = false
-  description = "Add the required for SPF and TXT records for mail to be sent from mailchimp."
-}
-
-variable fastmail_mailchimp_extra_spf {
-  type        = string
-  default     = null
-  description = "Mailchmp SPF record"
-}
-
-variable enable_mailgun {
-  type        = bool
-  default     = false
-  description = "Configure DNS for mailgun"
-}
-
 variable subdomain_name_servers {
   type        = map(list(string))
   default     = {}
