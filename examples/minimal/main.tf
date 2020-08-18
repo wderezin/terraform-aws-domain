76920793180386c223cedaa3d7538b918d17f2ac
Walter Derezinski <walter@daringway.com>
2020-08-17 23:58:56 -0400

    updated documentation

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -8,18 +8,7 @@ module domain {
   source      = "github.com/daringway/terraform-aws-domain-setup?ref=prod-test"
   domain_name = local.dns_zone_name
   tags        = local.tags
-}
 
-// Optional, will be integrated into module in terraform 13
-module fastmail {
-  source  = "github.com/daringway/terraform-aws-domain-setup//modules/fastmail?ref=prod-test"
-  zone_id = module.domain.zone.zone_id
-  tags    = local.tags
-}
-
-// Optional, will be integrated into module in terraform 13
-module mailgun {
-  source  = "github.com/daringway/terraform-aws-domain-setup//modules/mailgun?ref=prod-test"
-  zone_id = module.domain.zone.zone_id
-  tags    = local.tags
-}
+  enable_fastmail = true
+  enable_mailgun  = true
+}}
