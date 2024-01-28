95ec88512934d46c657f2ae32136abb30167e573
Walter Derezinski <walter@daringway.com>
2024-01-28 17:26:39 -0500

    Add new mailgun dkim krs DNS record

--- modules/mailgun/main.tf
+++ modules/mailgun/main.tf
@@ -21,6 +21,17 @@ resource aws_route53_record mailgun-dk {
   ]
 }
 
+resource aws_route53_record mailgun-dk2 {
+  zone_id = local.zone_id
+  name    = "krs._domainkey.mg.${local.domain_name}"
+  type    = "TXT"
+  ttl     = local.ttl
+
+  records = [
+    "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC5SixmhllsQ2bmm2+mZjW/mh3cNCs9J3w+F8Aey524FfYhV2qCGBK48eLyAVd5e0D2sakk1gWDMMIYD73SMwjcUc+C1K5GMYqws/+rR85AUuCXf91AmZZs+osyjv92cNKVUYJg4CiSDcUaN6WW6Hvarmqpv2cdNkK+LKqF8NLBfQIDAQAB"
+  ]
+}
+
 resource aws_route53_record mailgun-mx-0 {
   zone_id = local.zone_id
   name    = "mg.${local.domain_name}"
